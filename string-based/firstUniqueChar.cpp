a627f0279fd0ad6bc87e85878f5cb3bad83bd699
Akankshapandey181 <akankshapandey1106@gmail.com>
2026-03-14 13:50:11 +0530

    firstUniqueChar

---
+++ string-based/firstUniqueChar.cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    string s= "leetcode";
+    int freq[26]={0};
+    for(char c:s){
+        freq[c-'a']++;
+    }
+    for(char c:s){
+        if(freq[c-'a']==1){
+            cout<<c;
+            return 0;
+        }
+    }
+    cout<<-1;
+}
+
+
+
+
+// int main(){
+//     string s="leetcode";
+
+//     map<char,int>mp;
+//     for(char c:s){
+//         mp[c]++;
+//     }
+//     for(char c:s){
+//         if(mp[c]==1){
+//             cout<<c;
+//             return 0;
+//         }
+
+//     }
+//     cout<<-1;
+    
+//     }
+    
+    
